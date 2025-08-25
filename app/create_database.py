a558a77f30c6417ade4aadc50362ae03f1f749c0
jkmurphy123 <jkmurphy@zbzoom.net>
2025-08-25 11:22:16 -0400

    Fix typo

--- app/create_database.py
+++ app/create_database.py
@@ -1,4 +1,4 @@
-from .db import make_engine, create_db_and_tables, get_session, seed_projects_if_empty
+from db import make_engine, create_db_and_tables, get_session, seed_projects_if_empty
 
 engine = make_engine("sqlite:///spacegame.db", echo=False)
 create_db_and_tables(engine)
