dab083b15b0fa64d35ea89cd25a3992222e56d92
jkmurphy123 <jkmurphy@zbzoom.net>
2025-08-25 11:10:20 -0400

    added database create script

---
+++ app/utilities/create_database.py
@@ -0,0 +1,7 @@
+from .db import make_engine, create_db_and_tables, get_session, seed_projects_if_empty
+
+engine = make_engine("sqlite:///spacegame.db", echo=False)
+create_db_and_tables(engine)
+
+with get_session(engine) as s:
+    seed_projects_if_empty(s)
